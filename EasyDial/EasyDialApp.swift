5677d5d9d33e17c764a654dcee85f6bd853fac5b
rajesh <rajeshgotmail@gmail.com>
2025-09-13 23:43:06 -0700

    Initial Commit

---
+++ EasyDial/EasyDialApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyDialApp.swift
+//  EasyDial
+//
+//  Created by Rajesh Solanki on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct EasyDialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyDialTests/EasyDialTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyDialTests.swift
+//  EasyDialTests
+//
+//  Created by Rajesh Solanki on 9/13/25.
+//
+
+import Testing
+@testable import EasyDial
+
+struct EasyDialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
